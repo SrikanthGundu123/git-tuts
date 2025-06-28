6336aacbde31b80301bb4872fc85af454e251cf5
Srikanth Gundu <gundusrikanth16@gmail.com>
2025-06-29 00:02:21 +0530

    Commiting code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+# Addition of two numbers
+
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Addition
+sum = num1 + num2
+
+# Output result
+print("The sum is:", sum)
